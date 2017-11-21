366063bc991c6426a8d88b7b22494bc9e995b112
EagleBiceps <31891480+EagleBiceps@users.noreply.github.com>
2017-11-21 09:35:56 +0100

    hgsj

---
+++ Trojkat/src/Triangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+ Scanner scanner= new Scanner(System.in);
+ System.out.println("Podaj liczbe a : ");
+ a=scanner.nextInt();
+ System.out.println("Podaj liczbe a : ");
+ 
+ System.out.println("Podaj liczbe a : ");
+				
+	}
+
+}
